bff116ce311b70c46f78051d7b6b443ad4402a68
mweuro <20mehow01@gmail.com>
2025-06-24 15:37:19 +0200

    about + method merge

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import gradio as gr
 from src.models.pipeline import PIPELINE
 from src.content.transcription_tab import transcription_tab
 from src.content.about_tab import about_tab
-from src.content.contact_tab import contact_tab
 from src.content.front_image import image_tab
-from src.content.method_tab import method_tab
 from src.content.results_tab import results_tab
 from src.content.about_us_tab import about_us_tab
 
@@ -19,7 +17,6 @@ with gr.Blocks(theme='Taithrah/Minimal') as demo:
     image_tab()
     with gr.Tabs():
         about_tab()
-        method_tab()
         results_tab()
         transcription_tab(PIPELINE)
         about_us_tab()
--- src/content/contact_tab.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-def contact_tab():
-    with gr.TabItem("📩 Kontakt"):
-        gr.Markdown("## Formularz kontaktowy")
-        gr.Textbox(label="📧 Twój email")
-        gr.Textbox(label="💬 Wiadomość", lines=4)
-        gr.Button("✉️ Wyślij (do zaimplementowania)")
